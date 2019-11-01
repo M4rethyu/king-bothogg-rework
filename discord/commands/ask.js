a4bee8e43c8f3959e29fbe834bfe35c5d45cf029
Marethyu Dusk <maxischramm1@web.de>
2019-11-01 19:00:28 +0100

    Update ask.js

--- discord/commands/ask.js
+++ discord/commands/ask.js
@@ -34,7 +34,7 @@ exports.run = async (client, message, arguments, options, permission) => {
 exports.config = {
 	"cooldown" : 0,
 	"sharedCooldown" : true,
-	"permission" : 0,
+	"permission" : 5,
 	"syntax" : [
 		
 	],
