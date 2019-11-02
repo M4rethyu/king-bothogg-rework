198b187af0a1625ead5f7de1326f55f97d22438e
Marethyu Dusk <maxischramm1@web.de>
2019-11-02 02:33:24 +0100

    Update connected.js
    get summoner accounts at the start

--- twitch/events/connected.js
+++ twitch/events/connected.js
@@ -1,6 +1,10 @@
 module.exports = async (client, address, port) => {
 	client.log("log", "Twitch bot has started"); 
 	
+	// Get summoner accounts
+	client.erick.summonerAccounts = await client.getSummonerAccounts();
+	client.log("log", "loaded the LoL accounts: " + Array.from(client.erick.summonerAccounts, o => o.name).join(", "));
+	
 	for (const entry of client.twitch.tasks) {
 		client.taskTime(entry);
 		client.taskCooldown(entry);
